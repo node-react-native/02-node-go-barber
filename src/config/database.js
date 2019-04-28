42c9154ce4f54f58cf04ffdc6f9f7145985fe3b7
Carlos Mestre <carlos.mestre@gamersclub.com.br>
2019-04-28 19:49:40 -0300

    configure sequelize

--- src/config/database.js
+++ src/config/database.js
@@ -1,23 +1,13 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+module.exports = {
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  username: 'docker',
+  password: 'docker',
+  database: 'gonodemodulo2',
+  operatorAliases: false,
+  define: {
+    timestamps: true, // automatically add created_at and updated_at in each created table
+    underscored: true, // change camelcase to snakecase to field names
+    underscoredAll: true // apply the same to table names
   }
 }
