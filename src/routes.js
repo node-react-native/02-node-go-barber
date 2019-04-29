e48dbf6c8e0aee9c9dd558d199d5435e3c40c241
Carlos Mestre <carlos.mestre@gamersclub.com.br>
2019-04-28 22:44:02 -0300

    register user

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,13 @@ const express = require('express')
 
 const routes = express.Router()
 
+const UserController = require('./app/controllers/UserController')
+
 routes.get('/', (req, res) => {
-  res.render('auth/signup')
+  res.send('Hello')
 })
+
+routes.get('/signup', UserController.create)
+routes.post('/signup', UserController.store)
 
 module.exports = routes
